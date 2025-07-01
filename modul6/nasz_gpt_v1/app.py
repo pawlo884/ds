de8cbb676503c3505be6d8449ca8438c2a2be267
Paweł <pawlo884@gmail.com>
2025-07-01 12:09:18 +0200

    k

--- modul6/nasz_gpt_v1/app.py
+++ modul6/nasz_gpt_v1/app.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
 st.title(":parrot: Papuga!")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+for message in st.session_state['messages']:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("Wpisz co")
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    st.session_state['messages'].append(
+        {"role": "user", "content": prompt}
+    )
+
